cd58b6b6902c080fd5842a575a2bc3e0ab0b4ff0
zainulariff <80114394+zainularifff@users.noreply.github.com>
2026-06-15 22:02:58 +0800

    Stop loading broken report UI override

--- src/components/layout/AppShell.tsx
+++ src/components/layout/AppShell.tsx
@@ -2,7 +2,6 @@ import { useEffect } from "react";
 import { Outlet } from "react-router-dom";
 
 import { installDisplayCopyStandardizer } from "../../utils/displayCopy";
-import "../../styles/report-simple.css";
 import { Sidebar } from "./Sidebar";
 import { TopNavbar } from "./TopNavbar";
 
