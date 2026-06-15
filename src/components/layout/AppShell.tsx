402fb935b591cc37fed5ee25b1e9f54e451fbecc
zainulariff <80114394+zainularifff@users.noreply.github.com>
2026-06-16 00:45:23 +0800

    Load report card sizing styles

--- src/components/layout/AppShell.tsx
+++ src/components/layout/AppShell.tsx
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import { Outlet } from "react-router-dom";
 
 import { installDisplayCopyStandardizer } from "../../utils/displayCopy";
+import "../../styles/report-card-sizing.css";
 import { Sidebar } from "./Sidebar";
 import { TopNavbar } from "./TopNavbar";
 
