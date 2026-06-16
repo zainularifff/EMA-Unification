47545c5a18a62b7147237c73694e7d99158c0fe4
zainulariff <80114394+zainularifff@users.noreply.github.com>
2026-06-16 12:07:19 +0800

    Stabilize report page while ROI builder is prepared

--- src/pages/ReportBoard.tsx
+++ src/pages/ReportBoard.tsx
@@ -1,1 +1,8 @@
-export { default } from "./Report";
+export default function ReportBoard() {
+  return (
+    <main style={{ padding: 24 }}>
+      <h1>Report Builder</h1>
+      <p>ROI Software report pack is being prepared.</p>
+    </main>
+  );
+}
