439f959a59dd60a7e0c19168dbe4f3bbc212145d
zainulariff <80114394+zainularifff@users.noreply.github.com>
2026-06-16 13:18:56 +0800

    Install report date range enhancer

--- src/pages/ReportBoard.tsx
+++ src/pages/ReportBoard.tsx
@@ -1,3 +1,9 @@
+import { useEffect } from "react";
 import "../styles/report-builder-canvas-center.css";
+import { installReportDateRangeEnhancer } from "../utils/reportDateRangeEnhancer";
+import ReportBuilderRulesClean from "./ReportBuilderRulesClean";
 
-export { default } from "./ReportBuilderRulesClean";
+export default function ReportBoard() {
+  useEffect(() => installReportDateRangeEnhancer(), []);
+  return <ReportBuilderRulesClean />;
+}
