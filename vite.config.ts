a3feed1686f809fa8fb458d6577a4fdcbf09682c
zainulariff <80114394+zainularifff@users.noreply.github.com>
2026-06-18 23:30:01 +0800

    enable software drilldown analytics transform

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { itopsSoftwareDrilldownTransform } from './src/utils/itopsSoftwareDrilldownTransform';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [itopsSoftwareDrilldownTransform(), react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
