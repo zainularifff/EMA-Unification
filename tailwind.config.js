3ea538b068dbabb7674f26b217dfd6ef10126cf3
zainulariff <80114394+zainularifff@users.noreply.github.com>
2026-06-20 03:56:25 +0800

    Use standard Tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  prefix: "tw-",
-  corePlugins: {
-    preflight: false,
-  },
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
