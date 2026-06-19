a22d516c1282f7b2b962cc26394a8d1718e28bc8
zainulariff <80114394+zainularifff@users.noreply.github.com>
2026-06-20 03:18:03 +0800

    Import unified EMA table system

--- src/main.tsx
+++ src/main.tsx
@@ -24,6 +24,7 @@ import "./styles/management-control-settings.css";
 import "./styles/notification-channels.css";
 import "./styles/ema-system-shell.css";
 import "./styles/module-ui-fixes.css";
+import "./styles/ema-table-system.css";
 
 import App from "./App";
 import { AuthProvider } from "./context/AuthContext";
