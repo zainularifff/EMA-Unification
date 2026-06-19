33833389bb9011e13791089698d8d73bbe41b7d1
zainulariff <80114394+zainularifff@users.noreply.github.com>
2026-06-20 03:56:18 +0800

    Remove Bootstrap global imports

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,6 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle.min.js";
-
 import "./index.css";
 import "./styles/theme.css";
 import "./styles/app-global.css";
