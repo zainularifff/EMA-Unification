6a6f047b25bb3c594fc986ba96aa2cd7250ab743
zainulariff <80114394+zainularifff@users.noreply.github.com>
2026-06-20 13:05:58 +0800

    Remove extra EMA table stylesheet import

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,6 @@ import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import "./styles/ema-module-sidebar.css";
 import "./styles/ema-device-detail.css";
-import "./styles/ema-table.css";
 import "./styles/ema-modal.css";
 
 import App from "./App";
