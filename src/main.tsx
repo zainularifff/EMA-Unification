7ba916177643569b1d514f135466b6d9cd9e06a0
zainulariff <80114394+zainularifff@users.noreply.github.com>
2026-06-20 11:42:38 +0800

    Use shared EMA module sidebar stylesheet

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
 import "./index.css";
-import "./styles/hardware-sidebar-tabs.css";
+import "./styles/ema-module-sidebar.css";
 
 import App from "./App";
 import { AuthProvider } from "./context/AuthContext";
