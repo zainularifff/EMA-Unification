12949055721b5059624e4e65d688223bb57f4361
zainulariff <80114394+zainularifff@users.noreply.github.com>
2026-06-21 00:34:46 +0800

    Import EMA statistic styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,17 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import './index.css';
-import './components/ema/EmaRuntimeStyles';
-import './components/ema/EmaSidebarRuntimeStyles';
-import './components/ema/EmaToolbarRuntimeStyles';
-import './components/ema/EmaTableRuntimeStyles';
-import App from './App';
-import { AuthProvider } from './context/AuthContext';
-import { ThemeProvider } from './context/ThemeContext';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+import "./components/ema/EmaRuntimeStyles";
+import "./components/ema/EmaSidebarRuntimeStyles";
+import "./components/ema/EmaToolbarRuntimeStyles";
+import "./components/ema/EmaTableRuntimeStyles";
+import "./components/ema/EmaStatisticRuntimeStyles";
+import App from "./App";
+import { AuthProvider } from "./context/AuthContext";
+import { ThemeProvider } from "./context/ThemeContext";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider>
       <AuthProvider>
