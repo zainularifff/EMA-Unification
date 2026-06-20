6bc281e2d567762848f3ae6d31bd3ddca416bce9
zainulariff <80114394+zainularifff@users.noreply.github.com>
2026-06-20 22:56:51 +0800

    Load EMA runtime styles after base CSS

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import "./components/ema/EmaHardwareFixStyles";
 import "./index.css";
+import "./components/ema/EmaHardwareFixStyles";
 import App from "./App";
 import { AuthProvider } from "./context/AuthContext";
 import { ThemeProvider } from "./context/ThemeContext";
